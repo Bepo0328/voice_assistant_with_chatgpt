f4b66e07050f5797b7c1e43e77b1925f5d7b8a7e
Bepo0328 <82445568+Bepo0328@users.noreply.github.com>
2023-05-19 14:41:51 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# voice_assistant_with_chatgpt
+ ChatGPT를 이용한 음성 지원 앱
---
+++ android/app/src/main/kotlin/kr/co/bepo/voice_assistant_with_chatgpt/MainActivity.kt
@@ -0,0 +1,6 @@
+package kr.co.bepo.voice_assistant_with_chatgpt
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
